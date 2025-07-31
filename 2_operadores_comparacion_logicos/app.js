756e43c21907f0e2c2b91101efe39eac27b33110
DanielHincapie-cpu <hincapied286@gmail.com>
2025-07-31 13:28:45 -0500

    ejercicio terminado

--- 2_operadores_comparacion_logicos/app.js
+++ 2_operadores_comparacion_logicos/app.js
@@ -1,11 +1,11 @@
 //Boolean(...)
 
-console.log( ' "5" == "5" ', "5" == "5" ) //true
-console.log( ' "5" === "5" ', "5" === "5" ) //true
-console.log( ' 5 === "5" ', 5 == "5" ) //false
-console.log( ' 5 == "5" ', 5 == "5" ) //true
-console.log( ' 5 == (3+2) ', "5" == (3+2) ) //false 
-console.log( ' 5 == 5 ', 5 == 5 ) //true
+//console.log( ' "5" == "5" ', "5" == "5" ) //true
+//console.log( ' "5" === "5" ', "5" === "5" ) //true
+//console.log( ' 5 === "5" ', 5 == "5" ) //false
+//console.log( ' 5 == "5" ', 5 == "5" ) //true
+//console.log( ' 5 == (3+2) ', "5" == (3+2) ) //false 
+//console.log( ' 5 == 5 ', 5 == 5 ) //true
 
 //pida dos numeros al usuario y sumelos
 //let num1 = Number (prompt('Dame num 1')) //"5"
@@ -13,19 +13,28 @@ console.log( ' 5 == 5 ', 5 == 5 ) //true
 //let resultado = num1 + num2
 //alert("el resultado de su suma es " + resultado)
 
-let num1 = prompt("ingrese el primer numero(x):");
-let num2 = prompt("ingrese el segundo numero(y):");
+/* let operacion = prompt("¿Qué operación quieres hacer? (sumar, restar, multiplicar, dividir)");
+let num1 = parseFloat(prompt("Ingresa el primer número:"));
+let num2 = parseFloat(prompt("Ingresa el segundo número:"));
+let resultado;
 
-num1 = parseFloat(num1);
-num2 = parseFloat(num2);
+if (operacion === "sumar") {
+   resultado = num1 + num2;
+} else if (operacion === "restar") {
+  resultado = num1 - num2;
+} else if (operacion === "multiplicar") {
+    resultado = num1 * num2;
+} else if (operacion === "dividir") {
+    if (num2 !== 0) {
+        resultado = num1 / num2;
+    } else {
+        resultado = "Error: No se puede dividir por cero.";
+    }
+} else {
+  resultado = "Operación no válida.";
+}
 
-console.log("el valos x es = " + num1 + " y el valor y es = " + num2)
+alert("El resultado es: " + resultado);
+ */
 
-console.log("la suma de " + num1 + " + " + num2 + " = " + (num1 + num2));
-console.log("la resta de " + num1 + " - " + num2 + " = " + (num1 - num2));
-console.log("la multiplicacion de " + num1 + " * " + num2 + " = " + (num1 * num2));
-console.log("la division de " + num1 + " / " + num2 + " = " + (num1 / num2));
-
-
-let sumaMultiplicada = (num1 + num2) *10;
-console.log ("la suma de " + num1 + " + " + num2 + " multiplicado * 10 = " + sumaMultiplicada)
+console.log(`tu. numero es ${num1}`)
