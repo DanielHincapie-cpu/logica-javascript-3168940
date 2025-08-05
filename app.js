f51336db12b7b243c16301064088156584df6b36
DanielHincapie-cpu <hincapied286@gmail.com>
2025-08-05 13:26:35 -0500

    arrays en proceso

---
+++ app.js
@@ -0,0 +1,21 @@
+//array
+let frutas = ["mango", "piña", "papaya", "uva", "limon", "aguacate"]
+let jugadores = [
+{
+    id: 1,
+nombre: "diaz",
+pais: "col"
+},
+"james",
+"messi"
+]
+
+console.log( jugadores[0].nombre ) //imprimir
+
+//objeto
+let pelicula = { 
+nombre: "back to the future",
+estreno: 1986,
+director: "Robert Zemeckis",
+calificacion: "8.5",
+}
